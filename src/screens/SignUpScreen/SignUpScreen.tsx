a30d6f87e4cf69dafd7c401e149655c0e13636ba
okabAshish <ashishkmahtowork@gmail.com>
2023-04-26 15:36:27 +0530

    first commit

---
+++ src/screens/SignUpScreen/SignUpScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+type Props = {};
+
+const SignUpScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>SignUpScreen</Text>
+    </View>
+  );
+};
+
+export default SignUpScreen;
